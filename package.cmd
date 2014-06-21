7328fbf3754aa4ec26487c50f54d4da0c9d10e8d
Maximilian Haru Raditya <m4h4r4@gmail.com>
2014-06-22 05:03:07 +0700

    Signed (strongly-named) assemblies.

---
+++ package.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+SET BuildNumber=1000
+SET VersionControlInfo="master-Signed"
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:BuildNumber=%BuildNumber%,VersionControlInfo=%VersionControlInfo% Ciao.proj
+
+XCOPY "build\artifacts\*.nupkg" "..\..\Libraries\packages" /E /H /I /K /Y
+
+
+
